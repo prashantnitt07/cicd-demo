062842abe551e34dba0abf197d14926c49fe7651
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-07-05 18:54:24 +0530

    fb chagne Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port = 7000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Updated this file just now prashant singh, age: 20 years\n');
+
+  // First line
+  res.write('Updated this file just now prashant singh, age: 20 years\n');
+
+  // Additional output
+  res.write('This is the next code output line added to the response 3.2.1.\n');
+
+  res.end(); // End the response
 });
 
 server.listen(port, () => {
