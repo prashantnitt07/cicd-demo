d193db42adb35ff9a2e0bda5e29ff7486f0ea352
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-07-05 18:45:07 +0530

    Revert "fb version change Update index.js"

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Updated this file just now prashant singh, age: 20 years\n');
-  res.end('He is monitoring and obs engineer , fb version 3.2.1\n');
 });
 
 server.listen(port, () => {
